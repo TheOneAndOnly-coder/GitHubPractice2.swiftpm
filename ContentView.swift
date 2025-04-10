0eda768f54430d2d54bd0e1fa6aa2674aec9a98c
Ola Oleksiak <aoleksiak8215@stu.d214.org>
2025-04-10 10:58:54 -0500

    added more things

--- ContentView.swift
+++ ContentView.swift
@@ -5,11 +5,27 @@ struct ContentView: View {
     @State var number2 = 0
     @State var answer = 0
     var body: some View {
+        Text("Calculator")
+            .bold()
+        
         TextField("", value: $number1, format: .number )
         TextField("", value: $number2, format: .number)
-        Button("Add") {
-            answer = number1 + number2
+        HStack{
+            
+            
+            Button("Add") {
+                answer = number1 + number2
+            }
+            .foregroundStyle(.blue)
+            .padding()
+            Button("Subtract") {
+                answer = number1 - number2
+            }
+            .foregroundStyle(.cyan)
         }
+        .padding()
         Text("\(answer)")
+            .font(.largeTitle)
+            .foregroundStyle(.black)
     }
 }
