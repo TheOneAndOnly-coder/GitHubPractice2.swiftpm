ac161b6d2424323b8bd14aa9785603a7a1b00c00
Ola Oleksiak <aoleksiak8215@stu.d214.org>
2025-04-10 10:51:04 -0500

    deleted

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 5
-    @State var answer = 0
     var body: some View {
         TextField("", value: $number1, format: .number )
         TextField("", value: $number2, format: .number)
